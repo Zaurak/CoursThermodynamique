3f23cbd278ae0bcb7609e51dee3e40a32342f034
Quentin MARQUES <marques@ece.fr>
2013-01-08 20:07:40 +0100

    Ajout du support fusionné en un seul fichier de sortie.

---
+++ Todo/Todo.mm
@@ -0,0 +1,29 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1357668022532" ID="ID_31717721" MODIFIED="1357668041574" TEXT="Am&#xe9;lioration cours de Thermo">
+<node CREATED="1357668311242" ID="ID_994946494" MODIFIED="1357668323034" POSITION="left" TEXT="Validation du support">
+<node CREATED="1357668324852" ID="ID_1231925429" MODIFIED="1357668358449" TEXT="Orthographe/Grammaire"/>
+<node CREATED="1357668330553" ID="ID_1099899684" MODIFIED="1357668333696" TEXT="Equations"/>
+<node CREATED="1357668334451" ID="ID_1684974361" MODIFIED="1357668353470" TEXT="Validation finale (M. Gaudin)"/>
+</node>
+<node CREATED="1357668269106" ID="ID_591706622" MODIFIED="1357668278380" POSITION="left" TEXT="Ajouter fin du cours 7"/>
+<node CREATED="1357668122814" ID="ID_1043938222" MODIFIED="1357668918412" POSITION="right" TEXT="Corrections LaTeX">
+<node CREATED="1357668141297" ID="ID_1907477656" MODIFIED="1357668229235" TEXT="Ajouter r&#xe9;f&#xe9;rence vers Table des mati&#xe8;res"/>
+<node CREATED="1357668230690" ID="ID_1011855435" MODIFIED="1357668261569" TEXT="R&#xe9;gulariser liens internes vers les sch&#xe9;mas"/>
+<node CREATED="1357668286010" ID="ID_250558364" MODIFIED="1357668292554" TEXT="Enlever les pages blanches"/>
+<node CREATED="1357664247990" ID="ID_444476395" MODIFIED="1357668170697" TEXT="Enlever codes obsol&#xe8;tes">
+<node CREATED="1357664317343" ID="ID_1794958454" MODIFIED="1357668211092" TEXT="eqnarray* =&gt; align*"/>
+<node CREATED="1357668420928" ID="ID_68195057" MODIFIED="1357669068638" TEXT="mbox =&gt; &quot;&amp;&amp; commentaires&quot;"/>
+<node CREATED="1357664324790" ID="ID_737575188" MODIFIED="1357664333622" TEXT="\&apos;E{} =&gt; Unicode"/>
+</node>
+<node CREATED="1357668834205" ID="ID_1280273045" MODIFIED="1357668840977" TEXT="Uniformiser code source">
+<node CREATED="1357664844898" ID="ID_861308458" MODIFIED="1357668845471" TEXT="Remplacer tabulation 8 par 4 espaces"/>
+<node CREATED="1357664853463" ID="ID_207542031" MODIFIED="1357668859158" TEXT="Largeur du document &#xe0; 80 chars par ligne"/>
+</node>
+</node>
+<node CREATED="1357668365399" ID="ID_1954031981" MODIFIED="1357668393464" POSITION="left" TEXT="Clart&#xe9; du support">
+<node CREATED="1357668402135" ID="ID_1527465693" MODIFIED="1357668799250" TEXT="Ajouter des explications sur les &#xe9;quations"/>
+<node CREATED="1357668460268" ID="ID_1567903129" MODIFIED="1357668714937" TEXT="Ajouter des sch&#xe9;mas"/>
+</node>
+</node>
+</map>
